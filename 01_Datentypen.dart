b4d64e728783af48688cdf9c633e1331b005acb5
mclenz2018 <41286288+mclenz2018@users.noreply.github.com>
2020-07-07 11:50:25 +0200

    Create 01_Datentypen.dart

---
+++ 01_Datentypen.dart
@@ -0,0 +1,16 @@
+main(){
+  int zahl = 13;
+  double kommazahl = 13.4;
+  String name = "Ich poste Memes";
+
+  int nummer = 999999999;
+  double kommanummer = 999999999.99999;
+  String name2 = "HAHAHA";
+
+  print(zahl);
+  print(kommazahl);
+  print(name);
+  print(nummer);
+  print(kommanummer);
+  print(name2);
+}
